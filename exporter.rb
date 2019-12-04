0115e8654726e14dda7213a458e570b7113ff575
ichi-t <taisuke_ichimura@icloud.com>
2019-12-05 00:19:40 +0900

    Add humidity, illumination metrics

--- exporter.rb
+++ exporter.rb
@@ -3,18 +3,31 @@ require 'nature_remo'
 
 get '/metrics' do
   <<-"EOS"
-  #{temperture}
+  #{temperture}<br>
+  #{humidity}<br>
+  #{illumination}
   EOS
 end
 
 def temperture
+  "natureremo_temperture_celcius" + label + client.get_temp.to_s
+end
+
+def humidity
+  "natureremo_humidity" + label + client.get_humi.to_s
+end
+
+def illumination
+  "natureremo_illumination_lux" + label + client.get_illu.to_s
+end
+
+def label
   <<-"EOS"
-  natureremo_temperture{appliance_name="#{JSON.parse(client.appliances.body)[0]["device"]["name"]}",
-  appliance_id="#{JSON.parse(client.appliances.body)[0]["device"]["id"]}"} client.get_temp
+  {appliance_name="#{JSON.parse(client.appliances.body)[0]["device"]["name"]}",
+  appliance_id="#{JSON.parse(client.appliances.body)[0]["device"]["id"]}"}
   EOS
 end
 
 def client
-  client ||= NatureRemo::Client.
-               new(ENV['NATURE_TOKEN'])
+  client ||= NatureRemo::Client.new(ENV['NATURE_TOKEN'])
 end
